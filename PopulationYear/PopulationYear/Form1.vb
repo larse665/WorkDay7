cf6836aa1262ab577f2a20e932c98aafcce8729c
idsc3101 <idsc3101@gmail.com>
2014-10-08 17:11:39 -0500

    did workday 7 problems in class

--- PopulationYear/PopulationYear/Form1.vb
+++ PopulationYear/PopulationYear/Form1.vb
@@ -3,7 +3,13 @@
 Public Class Form1
 
     Private Sub btnPopulation_Click(sender As Object, e As EventArgs) Handles btnPopulation.Click
-        MessageBox.Show("The answer: not implemented yet!")
-
+        'MessageBox.Show("The answer: not implemented yet!")
+        Dim population As Double = 7000.0
+        Dim year As Integer = 2014
+        Do While (population > 6)
+            population = population / 2
+            year = year - 50
+        Loop
+        MessageBox.Show("The answer is " & year)
     End Sub
 End Class
